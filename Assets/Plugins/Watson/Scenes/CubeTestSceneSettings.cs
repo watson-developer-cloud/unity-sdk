4945aaaf6a75621d88042f3818bb2f7e97e9952d
Ajiemar Santiago <adsantiag@us.ibm.com>
2015-10-17 13:13:20 -0500

    format open cube

---
+++ Assets/Plugins/Watson/Scenes/CubeTestSceneSettings.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeTestSceneSettings : MonoBehaviour {
+//	private bool _isOpen = true;
+//	public bool isOpen {
+//		get { return _isOpen; }
+//		set
+//		{
+//			_isOpen = value;
+////			if(isOpen) 
+////			{
+////				openCube();
+////			} else {
+////				closeCube();
+////			}
+//		}
+//	}
+
+//	[SerializeField]
+//	private GameObject p0;
+//	[SerializeField]
+//	private GameObject p1;
+//	[SerializeField]
+//	private GameObject p2;
+//	[SerializeField]
+//	private GameObject p3;
+//	[SerializeField]
+//	private GameObject p4;
+//	[SerializeField]
+//	private GameObject p5;
+//
+//	private Quaternion p0_open = Quaternion.Euler (0f, 0f, 0f);//new Vector3(0f, 0f, 0f);
+//	private Quaternion p0_closed = Quaternion.Euler (0f, 90f, 0f);//new Vector3(0f, 0f, 0f);
+
+	void Start ()
+	{
+//		QualitySettings.antiAliasing = 16;
+	}
+
+	void Update()
+	{
+//		if (Input.GetKeyDown (KeyCode.Return)) {
+//			isOpen = !isOpen;
+//		}
+//
+//		if(isOpen)
+//		{
+////			p0.transform.rotation = Quaternion.Lerp(p0.transform.rotation, 
+//		}
+	}
+
+//	private void openCube()
+//	{
+//		Debug.Log ("open \tcube");
+//	}
+//
+//	private void closeCube()
+//	{
+//		Debug.Log ("close cube");
+//	}
+}
