113c80f4941f522256b4306d1b671f57cff18c09
Ajiemar Santiago <adsantiag@us.ibm.com>
2015-10-17 16:44:35 -0500

    box closing and opening

--- Assets/Plugins/Watson/Scenes/CubeTestSceneSettings.cs
+++ Assets/Plugins/Watson/Scenes/CubeTestSceneSettings.cs
@@ -31,10 +31,10 @@ public class CubeTestSceneSettings : MonoBehaviour {
 	[SerializeField]
 	private GameObject p5;
 
-	private Vector3 p0_position_open = new Vector3 (0f, 0f, 0f);
+	private Vector3 p0_position_open = new Vector3 (0f, 0f, 10f);
 	private Quaternion p0_rotation_open = Quaternion.Euler (0f, 0f, 0f);
-	private Vector3 p0_position_closed = new Vector3 (10f, 0f, -10f);
-	private Quaternion p0_rotation_closed = Quaternion.Euler (0f, -180f, 0f);
+	private Vector3 p0_position_closed = new Vector3 (0f, 0f, 10f);
+	private Quaternion p0_rotation_closed = Quaternion.Euler (0f, 90f, 0f);
 
 	private Vector3 p1_position_open = new Vector3 (0f, 0f, -10f);
 	private Quaternion p1_rotation_open = Quaternion.Euler (0f, 0f, 0f);
@@ -60,6 +60,36 @@ public class CubeTestSceneSettings : MonoBehaviour {
 	private Quaternion p5_rotation_open = Quaternion.Euler (0f, 0f, 0f);
 	private Vector3 p5_position_closed = new Vector3 (0f, -5f, -15f);
 	private Quaternion p5_rotation_closed = Quaternion.Euler (0f, 0f, 90f);
+
+//	private Vector3 p0_position_open = new Vector3 (0f, 0f, 5f);
+//	private Quaternion p0_rotation_open = Quaternion.Euler (90f, 270f, 0f);
+//	private Vector3 p0_position_closed = new Vector3 (10f, 0f, -15f);
+//	private Quaternion p0_rotation_closed = Quaternion.Euler (90f, 450f, 0f);
+//	
+//	private Vector3 p1_position_open = new Vector3 (0f, 0f, -5f);
+//	private Quaternion p1_rotation_open = Quaternion.Euler (90f, -90f, 0f);
+//	private Vector3 p1_position_closed = new Vector3 (5f, 0f, -10f);
+//	private Quaternion p1_rotation_closed = Quaternion.Euler (90f, 0f, 0f);
+//	
+//	private Vector3 p2_position_open = new Vector3 (0f, 0f, -15f);
+//	private Quaternion p2_rotation_open = Quaternion.Euler (90f, -90f, 0f);
+//	private Vector3 p2_position_closed = new Vector3 (0f, 0f, -15f);
+//	private Quaternion p2_rotation_closed = Quaternion.Euler (90f, -90f, 0f);
+//	
+//	private Vector3 p3_position_open = new Vector3 (0f, 0f, -25f);
+//	private Quaternion p3_rotation_open = Quaternion.Euler (90f, -90f, 0f);
+//	private Vector3 p3_position_closed = new Vector3 (5f, 0f, -20f);
+//	private Quaternion p3_rotation_closed = Quaternion.Euler (90f, -180f, 0f);
+//	
+//	private Vector3 p4_position_open = new Vector3 (0f, 10f, -15f);
+//	private Quaternion p4_rotation_open = Quaternion.Euler (90f, -90f, 0f);
+//	private Vector3 p4_position_closed = new Vector3 (5f, 5f, -15f);
+//	private Quaternion p4_rotation_closed = Quaternion.Euler (0f, -90f, 0f);
+//	
+//	private Vector3 p5_position_open = new Vector3 (0f, -	10f, -15f);
+//	private Quaternion p5_rotation_open = Quaternion.Euler (90f, -90f, 0f);
+//	private Vector3 p5_position_closed = new Vector3 (5f, -5f, -15f);
+//	private Quaternion p5_rotation_closed = Quaternion.Euler (0f, -270f, 180f);
 
 	void Start ()
 	{
@@ -93,6 +123,11 @@ public class CubeTestSceneSettings : MonoBehaviour {
 //		p5.transform.localPosition = (isOpen) ? p5.transform.localPositio`n = Quaternion.Slerp (p5.transform.localPosition, p5_position_open, Time.deltaTime * smooth) : p5.transform.localPosition = Quaternion.Slerp (p5.transform.localPosition, p5_position_closed, Time.deltaTime * smooth);
 
 		p0.transform.localPosition = (isOpen) ? Vector3.Lerp(p0.transform.localPosition, p0_position_open, Time.deltaTime * smooth) : Vector3.Lerp(p0.transform.localPosition, p0_position_closed, Time.deltaTime * smooth);
+		p1.transform.localPosition = (isOpen) ? Vector3.Lerp(p1.transform.localPosition, p1_position_open, Time.deltaTime * smooth) : Vector3.Lerp(p1.transform.localPosition, p1_position_closed, Time.deltaTime * smooth);
+		p2.transform.localPosition = (isOpen) ? Vector3.Lerp(p2.transform.localPosition, p2_position_open, Time.deltaTime * smooth) : Vector3.Lerp(p2.transform.localPosition, p2_position_closed, Time.deltaTime * smooth);
+		p3.transform.localPosition = (isOpen) ? Vector3.Lerp(p3.transform.localPosition, p3_position_open, Time.deltaTime * smooth) : Vector3.Lerp(p3.transform.localPosition, p3_position_closed, Time.deltaTime * smooth);
+		p4.transform.localPosition = (isOpen) ? Vector3.Lerp(p4.transform.localPosition, p4_position_open, Time.deltaTime * smooth) : Vector3.Lerp(p4.transform.localPosition, p4_position_closed, Time.deltaTime * smooth);
+		p5.transform.localPosition = (isOpen) ? Vector3.Lerp(p5.transform.localPosition, p5_position_open, Time.deltaTime * smooth) : Vector3.Lerp(p5.transform.localPosition, p5_position_closed, Time.deltaTime * smooth);
 	}
 
 //	private void openCube()
