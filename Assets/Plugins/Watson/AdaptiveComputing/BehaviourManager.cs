ba7f275a6ade75e3846dea1e38dad66b078d576b
Dogukan Erenel <derenel@us.ibm.com>
2015-10-17 14:49:28 -0500

    * Mood and Behavior color update according to Wilson's pdf

--- Assets/Plugins/Watson/AdaptiveComputing/BehaviourManager.cs
+++ Assets/Plugins/Watson/AdaptiveComputing/BehaviourManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using IBM.Watson.Utilities;
+using IBM.Watson.Logging;
 
 public class BehaviourManager {
 
@@ -21,29 +23,33 @@ public class BehaviourManager {
 		}
 	}
 
-	private Color m_currentBehaviourColor = null;
+	private Color m_currentBehaviourColor = Color.white;
 	public Color currentBehaviourColor{
 		get{
-			switch (currentBehaviour) {
-			case BehaviourType.Idle:
-				m_currentBehaviourColor =  new Color(241 / 255.0f, 241 / 255.0f, 242 / 255.0f);	break;
-			case BehaviourType.Listening:
-				m_currentBehaviourColor =  new Color(131 / 255.0f, 209 / 255.0f, 245 / 255.0f);	break;
-			case BehaviourType.Thinking:
-				m_currentBehaviourColor =  new Color(241 / 255.0f, 241 / 255.0f, 242 / 255.0f);	break;
-			case BehaviourType.Speaking:
-				m_currentBehaviourColor =  new Color(217 / 255.0f, 24 / 255.0f, 45 / 255.0f);	break;
-			case BehaviourType.DidntUnderstand:
-				m_currentBehaviourColor =  new Color(243 / 255.0f, 137 / 255.0f, 175 / 255.0f);	break;
-			default:
-			break;
-			}
 
+			switch (currentBehaviour) {
+				case BehaviourType.Idle:
+					m_currentBehaviourColor =  new Color(241 / 255.0f, 241 / 255.0f, 242 / 255.0f);	break;
+				case BehaviourType.Listening:
+					m_currentBehaviourColor =  new Color(0 / 255.0f, 166 / 255.0f, 160 / 255.0f);	break;
+				case BehaviourType.Thinking:
+					m_currentBehaviourColor =  new Color(238 / 255.0f, 62 / 255.0f, 150 / 255.0f);	break;
+				case BehaviourType.Speaking:
+					m_currentBehaviourColor =  new Color(140 / 255.0f, 198 / 255.0f, 63 / 255.0f);	break;
+				case BehaviourType.DidntUnderstand:
+					m_currentBehaviourColor =  new Color(243 / 255.0f, 137 / 255.0f, 175 / 255.0f);	break;
+				default:
+					Log.Error("BehaviourManager", "BehaviourType is not defined for color!");
+					m_currentBehaviourColor = Color.white;
+					break;
+				}
+			return m_currentBehaviourColor;
 		}
 	}
 
 	public void ChangeBehaviour(BehaviourType behaviourType){
 		m_currentBehaviour = behaviourType;
 		//TODO: Sent event to change behaviour!
+
 	}
 }
--- Assets/Plugins/Watson/AdaptiveComputing/MoodManager.cs
+++ Assets/Plugins/Watson/AdaptiveComputing/MoodManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using IBM.Watson.Logging;
+using IBM.Watson.Utilities;
 
 public class MoodManager{
 
@@ -19,6 +21,30 @@ public class MoodManager{
 		}
 		set{
 			ChangeMood(value);
+		}
+	}
+
+	private Color m_currentMoodColor = Color.white;
+	public Color currentMoodColor{
+		get{
+			
+			switch (currentMood) {
+			case MoodType.Idle:
+				m_currentMoodColor =  new Color(241 / 255.0f, 241 / 255.0f, 242 / 255.0f);	break;
+			case MoodType.Interested:
+				m_currentMoodColor =  new Color(131 / 255.0f, 209 / 255.0f, 245 / 255.0f);	break;
+			case MoodType.Urgent:
+				m_currentMoodColor =  new Color(241 / 255.0f, 241 / 255.0f, 242 / 255.0f);	break;
+			case MoodType.Upset:
+				m_currentMoodColor =  new Color(217 / 255.0f, 24 / 255.0f, 45 / 255.0f);	break;
+			case MoodType.Shy:
+				m_currentMoodColor =  new Color(243 / 255.0f, 137 / 255.0f, 175 / 255.0f);	break;
+			default:
+				Log.Error("MoodManager", "MoodType is not defined for color!");
+				m_currentMoodColor = Color.white;
+				break;
+			}
+			return m_currentMoodColor;
 		}
 	}
 
