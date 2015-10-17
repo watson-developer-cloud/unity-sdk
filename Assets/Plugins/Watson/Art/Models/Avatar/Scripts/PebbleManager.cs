29b456f81d6179c11567df3d7584ce220852e53b
Dogukan Erenel <derenel@us.ibm.com>
2015-10-17 10:35:07 -0500

    * Avatar 3D Model

---
+++ Assets/Plugins/Watson/Art/Models/Avatar/Scripts/PebbleManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using IBM.Watson.Logging;
+
+[System.Serializable]
+public class PebbleRow
+{
+	public GameObject[] pebbleList;
+}
+
+public class PebbleManager : MonoBehaviour {
+
+	public PebbleRow[] pebbleRowList = null;
+
+	// Use this for initialization
+	void Start () {
+		if (pebbleRowList == null) {
+			Log.Error("PebbleManager", "pebbleRowList is null! This shouldn't be!");
+			return;
+		}
+
+		LeanTween.init ( (int)(pebbleRowList.Length * pebbleRowList [0].pebbleList.Length * 1.5f));
+//		for (int i = 0; i < pebbleRowList.Length; i++) {
+//			if(pebbleRowList[i].pebbleList != null){
+//				for (int j = 0; j < pebbleRowList[i].pebbleList.Length; j++) {
+//					LeanTween.moveLocalY(pebbleRowList[i].pebbleList[j], Random.Range(-2.0f, 2.0f), Random.Range(0.5f, 1.0f)).setLoopPingPong();
+//				}
+//			}
+//			else{
+//				Log.Error("PebbleManager", "pebbleRowList[{0}].pebbleList is null", i);
+//			}
+//		}
+	}
+
+	public float timeLimit = 1.0f;
+	public float modifier = 1.0f;
+	public Vector2 smoothnessLimitBetweenRows; //Smothness MIN, MAX
+
+	// Update is called once per frame
+	void Update () {
+		float data = (Mathf.PingPong (Time.time, timeLimit) / timeLimit);
+		AddSoundData (data * modifier);
+	}
+
+	void AddSoundData(float centerHitNormalized){
+		for (int i = pebbleRowList.Length - 1; i >= 0; i--) {
+			float smoothnessBetweenRows = Mathf.Lerp(smoothnessLimitBetweenRows.y, smoothnessLimitBetweenRows.x, (float)i / pebbleRowList.Length);
+
+			if (pebbleRowList [i].pebbleList != null) {
+				for (int j = 0; j < pebbleRowList[i].pebbleList.Length; j++) {
+
+//					int numberOfPeaks = 3;
+//					float limitPeaks = (float) pebbleRowList[i].pebbleList.Length / numberOfPeaks;
+//					float smoothnessInRow = Mathf.Lerp(0.2f, 0.8f,  (Mathf.PingPong(j, limitPeaks) / limitPeaks) );
+
+					if(pebbleRowList[i].pebbleList[j] != null){
+
+						if( i > 0){
+						pebbleRowList[i].pebbleList[j].transform.localPosition = Vector3.Lerp(pebbleRowList[i].pebbleList[j].transform.localPosition, 
+							                                                                  new Vector3(pebbleRowList[i].pebbleList[j].transform.localPosition.x, pebbleRowList[i-1].pebbleList[j].transform.localPosition.y, pebbleRowList[i].pebbleList[j].transform.localPosition.z),
+							                                                                      smoothnessBetweenRows);
+						}
+						else{
+						pebbleRowList[i].pebbleList[j].transform.localPosition = Vector3.Lerp(pebbleRowList[i].pebbleList[j].transform.localPosition, 
+						                                                                      new Vector3(pebbleRowList[i].pebbleList[j].transform.localPosition.x, centerHitNormalized, pebbleRowList[i].pebbleList[j].transform.localPosition.z),
+							                                                                      smoothnessBetweenRows);
+						}
+						//LeanTween.cancel(pebbleRowList[i].pebbleList[j]);
+						//LeanTween.moveLocalY(pebbleRowList[i].pebbleList[j], centerHitNormalized, Time.deltaTime).setDelay(i * 0.5f);
+					}
+					else{
+						Log.Error("PebbleManager", "pebbleRowList[{0}].pebbleList[{1}].pebbleList is null", i, j);
+					}
+				}
+			}
+			else{
+				Log.Error("PebbleManager", "pebbleRowList[{0}].pebbleList is null", i);
+			}
+		}
+	}
+}
