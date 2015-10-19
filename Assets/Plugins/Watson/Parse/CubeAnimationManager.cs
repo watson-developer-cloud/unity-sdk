f3e605993f2ea343df52a3810faa68ae2a67910a
Dogukan Erenel <derenel@us.ibm.com>
2015-10-19 17:35:49 -0500

    * Animation is now under LeanTween hands = we have all customizations easy to implement * Widget Prefab minor modifications * Xray scene changes

--- Assets/Plugins/Watson/Parse/CubeAnimationManager.cs
+++ Assets/Plugins/Watson/Parse/CubeAnimationManager.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 using IBM.Watson.Utilities;
+using IBM.Watson.Logging;
 
 public class CubeAnimationManager : MonoBehaviour {
-	public float smooth = 2f;
-	public float statinoaryRotationSpeed = .01f;
+
+	public enum CubeSideType
+	{
+		Title = 0,
+		Answers,
+		Parse,
+		Evidence,
+		Location,
+		Chat
+	}
+
+	public float statinoaryRotationSpeed = 10.0f;
 	public Vector3  statinoaryRotationVector;
+	public float distanceFromCameraInZAfterUnfolding = 100f;
+	public Vector3 m_initialPosition;
 
 	public Camera mainCamera;
 	public Camera parseViewCamera;
 
 	private bool isRotating = true;
+
+	public float timeForFoldingUnfolding = 2.0f;
+	public LeanTweenType easeForFoldungUnfolding = LeanTweenType.easeInOutCubic;
 
 	private bool _isOpen = false;
 	public bool isUnFolding {
@@ -24,107 +40,100 @@ public class CubeAnimationManager : MonoBehaviour {
 
 	[Header("UI Faces")]
 	[SerializeField]
-	private GameObject p0;
-	[SerializeField]
-	private GameObject p1;
-	[SerializeField]
-	private GameObject p2;
-	[SerializeField]
-	private GameObject p3;
-	[SerializeField]
-	private GameObject p4;
-	[SerializeField]
-	private GameObject p5;
+	private GameObject[] uiFaceOnSide;
 
 	[Header("Render Textures")]
 	[SerializeField]
-	private RenderTexture renderTex_0;
-	[SerializeField]
-	private RenderTexture renderTex_1;
-	[SerializeField]
-	private RenderTexture renderTex_2;
-	[SerializeField]
-	private RenderTexture renderTex_3;
-	[SerializeField]
-	private RenderTexture renderTex_4;
-	[SerializeField]
-	private RenderTexture renderTex_5;
+	private RenderTexture[] renderTexSide;
 
-	private Vector3 p0_position_open = new Vector3 (0f, 0f, 10f);
-	private Quaternion p0_rotation_open = Quaternion.Euler (0f, 0f, 0f);
-	private Vector3 p0_position_closed = new Vector3 (0f, 0f, 10f);
-	private Quaternion p0_rotation_closed = Quaternion.Euler (0f, 90f, 0f);
+	private Vector3[] positionUnfold;
+	private Quaternion[] rotationUnfold;
+	private Vector3[] positionFold;
+	private Quaternion[] rotationFold;
 
-	private Vector3 p1_position_open = new Vector3 (-5f + 2.5f, 0f, 5f);
-	private Quaternion p1_rotation_open = Quaternion.Euler (0f, 0f, 0f);
-	private Vector3 p1_position_closed = new Vector3 (-5f, 0f, 5f);
-	private Quaternion p1_rotation_closed = Quaternion.Euler (0f, 90f, 0f);
+	private LTDescr[] animationPositionOnSide;
+	private LTDescr[] animationRotationOnSide;
+	private LTDescr animationRotationCube;
+	private LTDescr animationPositionCube;
 
-	private Vector3 p2_position_open = new Vector3 (-5f + 2.5f, 0f, -5f);
-	private Quaternion p2_rotation_open = Quaternion.Euler (0f, 0f, 0f);
-	private Vector3 p2_position_closed = new Vector3 (-5f, 0f, -5f);
-	private Quaternion p2_rotation_closed = Quaternion.Euler (0f, 0f, 0f);
-
-	private Vector3 p3_position_open = new Vector3 (-5f + 2.5f, 0f, -5f);
-	private Quaternion p3_rotation_open = Quaternion.Euler (0f, 0f, 0f);
-	private Vector3 p3_position_closed = new Vector3 (-5f, 0f, -5f);
-	private Quaternion p3_rotation_closed = Quaternion.Euler (0f, 270f, 0f);
-
-	private Vector3 p4_position_open = new Vector3 (-5f + 2.5f, 5f, 0f);
-	private Quaternion p4_rotation_open = Quaternion.Euler (0f, 0f, 0f);
-	private Vector3 p4_position_closed = new Vector3 (-5f, 5f, 0f);
-	private Quaternion p4_rotation_closed = Quaternion.Euler (0f, 0f, 270f);
-
-	private Vector3 p5_position_open = new Vector3 (-5f + 2.5f, -5f, 0f);
-	private Quaternion p5_rotation_open = Quaternion.Euler (0f, 0f, 0f);
-	private Vector3 p5_position_closed = new Vector3 (-5f, -5f, 0f);
-	private Quaternion p5_rotation_closed = Quaternion.Euler (0f, 0f, 90f);
-
-	void Start ()
+	void Awake ()
 	{
-		renderTex_0.useMipMap = true;
-		renderTex_1.useMipMap = true;
-		renderTex_2.useMipMap = true;
-		renderTex_3.useMipMap = true;
-		renderTex_4.useMipMap = true;
-		renderTex_5.useMipMap = true;
+		positionUnfold = new Vector3[]{
+			new Vector3 (0f, 0f, 10f), 
+			new Vector3 (-5f + 2.5f, 0f, 5f), 
+			new Vector3 (-5f + 2.5f, 0f, -5f), 
+			new Vector3 (-5f + 2.5f, 0f, -5f), 
+			new Vector3 (-5f + 2.5f, 5f, 0f), 
+			new Vector3 (-5f + 2.5f, -5f, 0f)};
 
-		checkFoldingStatus ();
+		rotationUnfold = new Quaternion[]{
+			Quaternion.Euler (0f, 0f, 0f),
+			Quaternion.Euler (0f, 0f, 0f),
+			Quaternion.Euler (0f, 0f, 0f),
+			Quaternion.Euler (0f, 0f, 0f),
+			Quaternion.Euler (0f, 0f, 0f),
+			Quaternion.Euler (0f, 0f, 0f)
+		};
+
+		positionFold = new Vector3[]{
+			new Vector3 (0f, 0f, 10f),
+			new Vector3 (-5f, 0f, 5f),
+			new Vector3 (-5f, 0f, -5f),
+			new Vector3 (-5f, 0f, -5f),
+			new Vector3 (-5f, 5f, 0f),
+			new Vector3 (-5f, -5f, 0f)
+		};
+
+		rotationFold = new Quaternion[]{
+			Quaternion.Euler (0f, 90f, 0f),
+			Quaternion.Euler (0f, 90f, 0f),
+			Quaternion.Euler (0f, 0f, 0f),
+			Quaternion.Euler (0f, 270f, 0f),
+			Quaternion.Euler (0f, 0f, 270f),
+			Quaternion.Euler (0f, 0f, 90f)
+		};
+
+		if (uiFaceOnSide.Length != renderTexSide.Length 
+			|| uiFaceOnSide.Length != positionUnfold.Length 
+			|| uiFaceOnSide.Length != rotationUnfold.Length 
+			|| uiFaceOnSide.Length != positionFold.Length 
+			|| uiFaceOnSide.Length != rotationFold.Length) {
+
+			Log.Error("CubeAnimationManager", "Cube Animation has some missing variables!");
+		}
+
+		for (int i = 0; i < renderTexSide.Length; i++) {
+			renderTexSide[i].useMipMap = true;
+		}
+
+		m_initialPosition = transform.position;
 	}
 
 	void Update()
 	{
 		if (Input.GetKeyDown (KeyCode.Return)) {
+			isUnFolding = !isUnFolding;
 
 			if(isUnFolding){
-				Fold();
-			}
-			else{
 				UnFold();
 			}
+			else{
+				Fold();
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+
+			//FocusOnSide();
 		}
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			isRotating = !isRotating;
 		}
 	
-		CubeFoldingAnimation ();
+		//CubeFoldingAnimation ();
 		CubeStatinoaryAnimation ();
 	}
-
-	#region Public Functions for Fold / Unfold / Focus On Side / Defocus etc.
-		
-	public void Fold(){
-		isUnFolding = false;
-
-	}
-	
-	public void UnFold(){
-		isUnFolding = true;
-	}
-
-	#endregion
-
 
 	private void CubeStatinoaryAnimation(){
 		if (isRotating && !isUnFolding) {	//If it is fold already and rotation is true we are rotating.
@@ -132,98 +141,75 @@ public class CubeAnimationManager : MonoBehaviour {
 		}
 	}
 
-	public bool m_isAlreadyUnfold = false;
-	public bool m_isAlreadyFold = false;
+	#region Public Functions for Fold / Unfold / Focus On Side / Defocus etc.
+		
+	public void Fold(){
 
-	#region Folding - Unfolding Animations
+		CubeAnimationStop ();
 
-	private void CubeFoldingAnimation(){
+		for (int i = 0; i < uiFaceOnSide.Length; i++) {
+			animationPositionOnSide[i] = LeanTween.moveLocal (uiFaceOnSide[i], positionFold[i], timeForFoldingUnfolding).setEase (easeForFoldungUnfolding);
+			animationRotationOnSide[i] = LeanTween.rotateLocal (uiFaceOnSide[i], rotationFold[i].eulerAngles, timeForFoldingUnfolding).setEase (easeForFoldungUnfolding);
+		}
 
-		if (isUnFolding && !m_isAlreadyUnfold) {	//UnFold the Cube
-			CubeUnfoldOnUpdate ();
-			CheckUnfoldOnUpdateRotation ();
-			checkFoldingStatus ();
-		}else if(isUnFolding && m_isAlreadyUnfold){
-			CheckUnfoldOnUpdateRotation();
-		} else if (!isUnFolding && !m_isAlreadyFold) {	//Fold the Cube
-			CubeFoldOnUpdate();
-			checkFoldingStatus();
+		//Move object cloase to camera
+		animationPositionCube = LeanTween.move (gameObject, m_initialPosition, timeForFoldingUnfolding);
+	}
+	
+	public void UnFold(){
+		CubeAnimationStop ();
+		
+		for (int i = 0; i < uiFaceOnSide.Length; i++) {
+			animationPositionOnSide[i] = LeanTween.moveLocal (uiFaceOnSide[i], positionUnfold[i], timeForFoldingUnfolding).setEase (easeForFoldungUnfolding);
+			animationRotationOnSide[i] = LeanTween.rotateLocal (uiFaceOnSide[i], rotationUnfold[i].eulerAngles, timeForFoldingUnfolding).setEase (easeForFoldungUnfolding);
+		}
+
+		//Rotate to camera 
+		animationRotationCube = LeanTween.rotateLocal(gameObject, parseViewCamera.transform.rotation.eulerAngles, timeForFoldingUnfolding).setEase(LeanTweenType.easeInOutCubic);
+
+		//Move object cloase to camera
+		animationPositionCube = LeanTween.move (gameObject, parseViewCamera.transform.position + distanceFromCameraInZAfterUnfolding * parseViewCamera.transform.forward, timeForFoldingUnfolding).setEase(LeanTweenType.easeInOutCubic);
+	}
+
+	private void CubeAnimationStop(){
+		if (animationPositionOnSide == null) {
+			animationPositionOnSide = new LTDescr[uiFaceOnSide.Length];
 		} else {
-			// Do nothing - It is already unfold or aldready fold, no need to do more action!
+			for (int i = 0; i < animationPositionOnSide.Length; i++) {
+				LeanTween.cancel(animationPositionOnSide[i].uniqueId);
+			}
+		}
+		
+		if (animationRotationOnSide == null) {
+			animationRotationOnSide = new LTDescr[uiFaceOnSide.Length];
+		} else {
+			for (int i = 0; i < animationRotationOnSide.Length; i++) {
+				LeanTween.cancel(animationRotationOnSide[i].uniqueId);
+			}
+		}
+
+		if (animationRotationCube != null) {
+			LeanTween.cancel(animationRotationCube.uniqueId);
+		}
+
+		if (animationPositionCube != null) {
+			LeanTween.cancel (animationPositionCube.uniqueId);
 		}
 	}
 
+	#endregion
 
-	public void checkFoldingStatus(){
-		m_isAlreadyUnfold = 
-				//Checking the rotation
-				Utility.CheckEqualityQuaternion(p0.transform.localRotation, p0_rotation_open) && 
-				Utility.CheckEqualityQuaternion(p1.transform.localRotation, p1_rotation_open) &&
-				Utility.CheckEqualityQuaternion(p2.transform.localRotation, p2_rotation_open) &&
-				Utility.CheckEqualityQuaternion(p3.transform.localRotation, p3_rotation_open) &&
-				Utility.CheckEqualityQuaternion(p4.transform.localRotation, p4_rotation_open) &&
-				Utility.CheckEqualityQuaternion(p5.transform.localRotation, p5_rotation_open) &&
-				//Checking the position
-				p0.transform.localPosition == p0_position_open && 
-				p1.transform.localPosition == p1_position_open &&
-				p2.transform.localPosition == p2_position_open &&
-				p3.transform.localPosition == p3_position_open &&
-				p4.transform.localPosition == p4_position_open &&
-				p5.transform.localPosition == p5_position_open ;
 
-		m_isAlreadyFold = 
-				//Checking the rotation
-				Utility.CheckEqualityQuaternion(p0.transform.localRotation, p0_rotation_closed) && 
-				Utility.CheckEqualityQuaternion(p1.transform.localRotation, p1_rotation_closed) &&
-				Utility.CheckEqualityQuaternion(p2.transform.localRotation, p2_rotation_closed) &&
-				Utility.CheckEqualityQuaternion(p3.transform.localRotation, p3_rotation_closed) &&
-				Utility.CheckEqualityQuaternion(p4.transform.localRotation, p4_rotation_closed) &&
-				Utility.CheckEqualityQuaternion(p5.transform.localRotation, p5_rotation_closed) &&
-				//Checking the position
-				p0.transform.localPosition == p0_position_closed && 
-				p1.transform.localPosition == p1_position_closed &&
-				p2.transform.localPosition == p2_position_closed &&
-				p3.transform.localPosition == p3_position_closed &&
-				p4.transform.localPosition == p4_position_closed &&
-				p5.transform.localPosition == p5_position_closed ;
-	}
+	#region Folding - Unfolding Animations
 
-	private void CubeUnfoldOnUpdate(){
-		p0.transform.localRotation = Quaternion.Slerp (p0.transform.localRotation, p0_rotation_open, Time.deltaTime * smooth);
-		p1.transform.localRotation = Quaternion.Slerp (p1.transform.localRotation, p1_rotation_open, Time.deltaTime * smooth);
-		p2.transform.localRotation = Quaternion.Slerp (p2.transform.localRotation, p2_rotation_open, Time.deltaTime * smooth);
-		p3.transform.localRotation = Quaternion.Slerp (p3.transform.localRotation, p3_rotation_open, Time.deltaTime * smooth);
-		p4.transform.localRotation = Quaternion.Slerp (p4.transform.localRotation, p4_rotation_open, Time.deltaTime * smooth);
-		p5.transform.localRotation = Quaternion.Slerp (p5.transform.localRotation, p5_rotation_open, Time.deltaTime * smooth);
-		
-		p0.transform.localPosition = Vector3.Lerp(p0.transform.localPosition, p0_position_open, Time.deltaTime * smooth);
-		p1.transform.localPosition = Vector3.Lerp(p1.transform.localPosition, p1_position_open, Time.deltaTime * smooth);
-		p2.transform.localPosition = Vector3.Lerp(p2.transform.localPosition, p2_position_open, Time.deltaTime * smooth);
-		p3.transform.localPosition = Vector3.Lerp(p3.transform.localPosition, p3_position_open, Time.deltaTime * smooth);
-		p4.transform.localPosition = Vector3.Lerp(p4.transform.localPosition, p4_position_open, Time.deltaTime * smooth);
-		p5.transform.localPosition = Vector3.Lerp(p5.transform.localPosition, p5_position_open, Time.deltaTime * smooth);
-	}
+	public void FocusOnSide(CubeSideType sideType){
+		if(uiFaceOnSide.Length < (int) sideType){
+			Log.Error("CubeAnimationManager", "CubeAnimationManager - FocusOnSide {0} has wrong number as side. ", (int) sideType);
+			return;
+		}
 
-	private void CubeFoldOnUpdate(){
-		p0.transform.localRotation = Quaternion.Slerp (p0.transform.localRotation, p0_rotation_closed, Time.deltaTime * smooth);
-		p1.transform.localRotation = Quaternion.Slerp (p1.transform.localRotation, p1_rotation_closed, Time.deltaTime * smooth);
-		p2.transform.localRotation = Quaternion.Slerp (p2.transform.localRotation, p2_rotation_closed, Time.deltaTime * smooth);
-		p3.transform.localRotation = Quaternion.Slerp (p3.transform.localRotation, p3_rotation_closed, Time.deltaTime * smooth);
-		p4.transform.localRotation = Quaternion.Slerp (p4.transform.localRotation, p4_rotation_closed, Time.deltaTime * smooth);
-		p5.transform.localRotation = Quaternion.Slerp (p5.transform.localRotation, p5_rotation_closed, Time.deltaTime * smooth);
-		
-		p0.transform.localPosition = Vector3.Lerp(p0.transform.localPosition, p0_position_closed, Time.deltaTime * smooth);
-		p1.transform.localPosition = Vector3.Lerp(p1.transform.localPosition, p1_position_closed, Time.deltaTime * smooth);
-		p2.transform.localPosition = Vector3.Lerp(p2.transform.localPosition, p2_position_closed, Time.deltaTime * smooth);
-		p3.transform.localPosition = Vector3.Lerp(p3.transform.localPosition, p3_position_closed, Time.deltaTime * smooth);
-		p4.transform.localPosition = Vector3.Lerp(p4.transform.localPosition, p4_position_closed, Time.deltaTime * smooth);
-		p5.transform.localPosition = Vector3.Lerp(p5.transform.localPosition, p5_position_closed, Time.deltaTime * smooth);
 
-	}
 
-	private void CheckUnfoldOnUpdateRotation(){
-		transform.localRotation = Quaternion.Slerp (transform.localRotation, parseViewCamera.transform.rotation, Time.deltaTime * smooth);
-		//LeanTween.rotateLocal (gameObject, parseViewCamera.transform.rotation.eulerAngles , smooth);
 	}
 
 	#endregion
