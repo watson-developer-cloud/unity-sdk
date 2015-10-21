eaa3a837f6abcb6df01616b0305198036dc6aeb0
Ajiemar Santiago <adsantiag@us.ibm.com>
2015-10-20 23:39:12 -0500

    evidence

--- Assets/Plugins/Watson/Parse/Evidence.cs
+++ Assets/Plugins/Watson/Parse/Evidence.cs
@@ -4,8 +4,12 @@ using System.Collections.Generic;
 using IBM.Watson.Utilities;
 
 public class Evidence : QuestionComponentBase {
-	private List<EvidenceItem> m_EvidenceItems = new List<EvidenceItem>();
+	private List<EvidenceItem> EvidenceItems = new List<EvidenceItem>();
 	private ObservedList<string> m_EvidenceData = new ObservedList<string>();
+
+	[Header("Evidence")]
+	[SerializeField]
+	private EvidenceItem[] m_EvidenceItems;
 
 	new void Start()
 	{
@@ -14,6 +18,18 @@ public class Evidence : QuestionComponentBase {
 
 	new public void Init()
 	{
+		base.Init ();
+
+		//	TODO do this correctly
+		if(qWidget.Answers.answers[0].evidence.Length < 2) {
+			m_EvidenceItems[0].m_Evidence = qWidget.Answers.answers[0].evidence[0].title;
+			m_EvidenceItems[1].gameObject.SetActive(false);
+		} else {
+			m_EvidenceItems[1].gameObject.SetActive(true);
+			m_EvidenceItems[0].m_Evidence = qWidget.Answers.answers[0].evidence[0].title;
+			m_EvidenceItems[1].m_Evidence = qWidget.Answers.answers[0].evidence[1].title;
+		}
+
 		m_EvidenceData.Added += onAdd;
 	}
 
@@ -22,7 +38,7 @@ public class Evidence : QuestionComponentBase {
 		base.Init ();
 		Debug.Log ("evidence added");
 		EvidenceItem evidenceItem = new EvidenceItem ();
-		m_EvidenceItems.Add (evidenceItem);
+		EvidenceItems.Add (evidenceItem);
 		evidenceItem.m_Evidence = m_EvidenceData [m_EvidenceData.Count];
 	}
 }
