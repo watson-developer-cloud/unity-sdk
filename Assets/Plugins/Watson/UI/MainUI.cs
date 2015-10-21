fd840e5aa52dcd80a146857fcf2443a43526bbda
Dogukan Erenel <derenel@us.ibm.com>
2015-10-21 10:56:25 -0500

    * Changed MainUI script and added all Touch scenes to test

--- Assets/Plugins/Watson/UI/MainUI.cs
+++ Assets/Plugins/Watson/UI/MainUI.cs
@@ -44,4 +44,94 @@ public class MainUI : MonoBehaviour {
     {
         Application.LoadLevel( "Main" );
     }
+
+	public void OnTouchTestCamera()
+	{
+		Application.LoadLevel( "Camera" );
+	}
+	public void OnTouchTestCheckers()
+	{
+		Application.LoadLevel( "Checkers" );
+	}
+	public void OnTouchTestColors()
+	{
+		Application.LoadLevel( "Colors" );
+	}
+	public void OnTouchTestInput()
+	{
+		Application.LoadLevel( "Input" );
+	}
+	public void OnTouchTestMultiUser()
+	{
+		Application.LoadLevel( "Multiuser" );
+	}
+	public void OnTouchTestPhotos()
+	{
+		Application.LoadLevel( "Photos" );
+	}
+	public void OnTouchTestPhotos2D()
+	{
+		Application.LoadLevel( "Photos2D" );
+	}
+	public void OnTouchTestPortal()
+	{
+		Application.LoadLevel( "Portal" );
+	}
+	public void OnTouchTestTaps()
+	{
+		Application.LoadLevel( "Taps" );
+	}
+	public void OnTouchTestUI()
+	{
+		Application.LoadLevel( "InputModule" );
+	}
+
+	public void OnBack()
+	{
+		if (Application.loadedLevelName != "Main") {
+			OnMain ();
+		} else {
+			Application.Quit();
+		}
+	}
+
+	private static MainUI _instance = null;
+	void Awake() {
+		if (!_instance) {	//first-time opening
+			_instance = this;
+		} else if (_instance != this) {
+			Destroy (_instance.gameObject);
+			_instance = this;
+			MakeActiveEventSystem(false);
+			StartCoroutine(MakeActiveEventSystemWithDelay(true));
+
+		}
+		else {
+			//do nothing - the other instance will destroy the current instance.
+		}
+
+		DontDestroyOnLoad(transform.gameObject);
+
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+
+	IEnumerator MakeActiveEventSystemWithDelay(bool active){
+		yield return new WaitForEndOfFrame ();
+		MakeActiveEventSystem (active);
+	}
+	void MakeActiveEventSystem(bool active){
+		Transform[] transformList = gameObject.GetComponentsInChildren<Transform>(true);
+		for (int i = 0; i < transformList.Length; i++) {
+			if(transformList[i].name == "EventSystem"){
+				transformList[i].gameObject.SetActive(active);
+				break;
+			}
+		}
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.LeftArrow) ||	Input.GetKeyDown (KeyCode.Escape)) {
+			OnBack();
+		} 
+	}
 }
