bf841e068cbf49fb960bc5c25dff052c9a24cfcc
Ajiemar Santiago <adsantiag@us.ibm.com>
2015-11-02 08:15:22 -0600

    git status

--- Assets/Watson/Widgets/Question/Elements/PassageItem.cs
+++ Assets/Watson/Widgets/Question/Elements/PassageItem.cs
@@ -8,11 +8,11 @@ namespace IBM.Watson.Widgets.Question
 	/// Controls PassageItem view.
 	/// </summary>
 	public class PassageItem : MonoBehaviour {
-		/// <summary>
-		/// The passage text.
-		/// </summary>
 		[SerializeField]
 		private Text m_PassageText;
+
+		[SerializeField]
+		private TabItem m_TabItem;
 
 		private string m_Passage;
 		public string Passage
@@ -25,6 +25,39 @@ namespace IBM.Watson.Widgets.Question
 			}
 		}
 
+		private double m_Confidence = 0f;
+		public string Confidence
+		{
+			get { return m_Confidence; }
+			set
+			{
+				m_Confidence = value;
+				UpdateConfidence();
+			}
+		}
+
+		private double m_MaxConfidence = 1f;
+		public double MaxConfidence
+		{
+			get { return m_MaxConfidence; }
+			set
+			{
+				m_MaxConfidence = value;
+				UpdateConfidence();
+			}
+		}
+		
+		private double m_MinConfidence = 0f;
+		public double MinConfidence
+		{
+			get { return m_MinConfidence; }
+			set
+			{
+				m_MinConfidence = value;
+				UpdateConfidence();
+			}
+		}
+
 		/// <summary>
 		/// Update the passage view.
 		/// </summary>
@@ -32,6 +65,14 @@ namespace IBM.Watson.Widgets.Question
 		{
 			//	TODO format passage
 			m_PassageText.text = Passage;
+		}
+
+		/// <summary>
+		/// Update the confidence tab view.
+		/// </summary>
+		private void UpdateConfidence()
+		{
+
 		}
 	}
 }
