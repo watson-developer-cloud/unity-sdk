de21f777378af42df99f7e8abcdf94dcad0d043d
Ajiemar Santiago <adsantiag@us.ibm.com>
2015-11-02 15:21:02 -0600

    posiitioning passageItems

--- Assets/Watson/Widgets/Question/Elements/PassageItem.cs
+++ Assets/Watson/Widgets/Question/Elements/PassageItem.cs
@@ -1,6 +1,25 @@
-﻿using UnityEngine;
+﻿/**
+* Copyright 2015 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* @author Taj Santiago
+*/
+
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using IBM.Watson.Logging;
 
 namespace IBM.Watson.Widgets.Question
 {
@@ -48,7 +67,7 @@ namespace IBM.Watson.Widgets.Question
 			set
 			{
 				m_MaxConfidence = value;
-				UpdateConfidence();
+//				UpdateConfidence();
 			}
 		}
 		
@@ -59,7 +78,7 @@ namespace IBM.Watson.Widgets.Question
 			set
 			{
 				m_MinConfidence = value;
-				UpdateConfidence();
+//				UpdateConfidence();
 			}
 		}
 
@@ -83,6 +102,7 @@ namespace IBM.Watson.Widgets.Question
 			double ConfidenceRange = MaxConfidence - MinConfidence;
 			float ConfidencePercentage = (float)NormalizedConfidence/(float)ConfidenceRange;
 
+//			m_TabPosY = ((m_MaxYPos - m_MinYPos) * ConfidencePercentage) + m_MinYPos;
 			m_TabPosY = ((m_MaxYPos - m_MinYPos) * ConfidencePercentage) + m_MinYPos;
 			RectTransform TabItemRectTransform = m_TabItem.GetComponent<RectTransform>();
 			TabItemRectTransform.anchoredPosition = new Vector2(m_TabPosX, m_TabPosY);
--- Assets/Watson/Widgets/Question/Facet/Passages.cs
+++ Assets/Watson/Widgets/Question/Facet/Passages.cs
@@ -1,4 +1,22 @@
-﻿using UnityEngine;
+﻿/**
+* Copyright 2015 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* @author Taj Santiago
+*/
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -28,14 +46,18 @@ namespace IBM.Watson.Widgets.Question
 		{
 			base.Init ();
 
-			for(int i = 0; i < m_Question.QuestionData.AnswerDataObject.answers[0].evidence.Length; i++) {
-				GameObject PassageItemGameObject = Instantiate(m_PassageItemPrefab, new Vector3(m_RectTransformPosX, m_RectTransformPosY, m_RectTransformPosZ + m_RectTransformZSpacing * i), Quaternion.identity) as GameObject;
+			for(int i = 0; i < m_Question.QuestionData.AnswerDataObject.answers.Length ; i++) {
+				GameObject PassageItemGameObject = Instantiate(m_PassageItemPrefab, new Vector3(m_RectTransformPosX, m_RectTransformPosY, m_RectTransformPosZ + m_RectTransformZSpacing * (m_Question.QuestionData.AnswerDataObject.answers.Length - i)), Quaternion.identity) as GameObject;
 				RectTransform PassageItemRectTransform = PassageItemGameObject.GetComponent<RectTransform>();
 				PassageItemRectTransform.SetParent(m_PassageCanvasRectTransform, false);
 				PassageItem PassageItem = PassageItemGameObject.GetComponent<PassageItem>();
+				PassageItemRectTransform.SetAsFirstSibling();
 				m_PassageItems.Add(PassageItem);
-				PassageItem.PassageString = m_Question.QuestionData.AnswerDataObject.answers[0].evidence[i].passage;
-				PassageItem.Confidence = m_Question.QuestionData.AnswerDataObject.answers[0].confidence;
+//				PassageItem.PassageString = m_Question.QuestionData.AnswerDataObject.answers[0].evidence[i].passage;
+				PassageItem.PassageString = m_Question.QuestionData.AnswerDataObject.answers[i].answerText;
+				PassageItem.MaxConfidence = m_Question.QuestionData.AnswerDataObject.answers[0].confidence;
+				PassageItem.MinConfidence = m_Question.QuestionData.AnswerDataObject.answers[m_Question.QuestionData.AnswerDataObject.answers.Length - 1].confidence;
+				PassageItem.Confidence = m_Question.QuestionData.AnswerDataObject.answers[i].confidence;
 			}
 		}
 		
