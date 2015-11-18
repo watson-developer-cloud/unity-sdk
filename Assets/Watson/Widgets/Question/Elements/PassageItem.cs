11ffbf75e2b93f3fd2ecf841959c6f0cccd107f2
Richard Lyle <rolyle@us.ibm.com>
2015-11-18 15:32:03 -0600

    * Fixed crash in Evidence * Cleaned up Passages & PassageItem classes. * Added support for numerati_woodside, this needs to be data driven.

--- Assets/Watson/Widgets/Question/Elements/PassageItem.cs
+++ Assets/Watson/Widgets/Question/Elements/PassageItem.cs
@@ -23,89 +23,96 @@ using IBM.Watson.Logging;
 
 namespace IBM.Watson.Widgets.Question
 {
-	/// <summary>
-	/// Controls PassageItem view.
-	/// </summary>
-	public class PassageItem : MonoBehaviour {
-		private float m_MaxYPos = 480f;
-		private float m_MinYPos = -250f;
-		private float m_TabPosX = -160f;
-		private float m_TabPosY;
+    /// <summary>
+    /// Controls PassageItem view.
+    /// </summary>
+    public class PassageItem : MonoBehaviour
+    {
+        private float m_MaxYPos = 480f;
+        private float m_MinYPos = -250f;
+        private float m_TabPosX = -160f;
+        private float m_TabPosY;
 
-		[SerializeField]
-		private Text m_PassageText;
+        [SerializeField]
+        private Text m_PassageText = null;
+        [SerializeField]
+        private TabItem m_TabItem = null;
 
-		[SerializeField]
-		private TabItem m_TabItem;
+        private string m_PassageString;
+        public string PassageString
+        {
+            get { return m_PassageString; }
+            set
+            {
+                m_PassageString = value;
+                UpdatePassage();
+            }
+        }
+        private double m_Confidence = 0f;
+        public double Confidence
+        {
+            get { return m_Confidence; }
+            set
+            {
+                m_Confidence = value;
+                UpdateConfidence();
+            }
+        }
 
-		private string m_PassageString;
-		public string PassageString
-		{
-			get { return m_PassageString; }
-			set
-			{
-				m_PassageString = value;
-				UpdatePassage();
-			}
-		}
+        private double m_MaxConfidence = 1f;
+        public double MaxConfidence
+        {
+            get { return m_MaxConfidence; }
+            set
+            {
+                m_MaxConfidence = value;
+            }
+        }
 
-		private double m_Confidence = 0f;
-		public double Confidence
-		{
-			get { return m_Confidence; }
-			set
-			{
-				m_Confidence = value;
-				UpdateConfidence();
-			}
-		}
+        private double m_MinConfidence = 0f;
+        public double MinConfidence
+        {
+            get { return m_MinConfidence; }
+            set
+            {
+                m_MinConfidence = value;
+            }
+        }
 
-		private double m_MaxConfidence = 1f;
-		public double MaxConfidence
-		{
-			get { return m_MaxConfidence; }
-			set
-			{
-				m_MaxConfidence = value;
-//				UpdateConfidence();
-			}
-		}
-		
-		private double m_MinConfidence = 0f;
-		public double MinConfidence
-		{
-			get { return m_MinConfidence; }
-			set
-			{
-				m_MinConfidence = value;
-//				UpdateConfidence();
-			}
-		}
+        /// <summary>
+        /// Update the passage view.
+        /// </summary>
+        private void UpdatePassage()
+        {
+            //	TODO format passage
+            m_PassageText.text = PassageString;
+            // wait for the frame to update before we update the scroll position
+            Invoke( "ScrollToTop", 0.5f );
+        }
 
-		/// <summary>
-		/// Update the passage view.
-		/// </summary>
-		private void UpdatePassage()
-		{
-			//	TODO format passage
-			m_PassageText.text = PassageString;
-		}
+        private void ScrollToTop()
+        {
+            ScrollRect scrollRect = GetComponentInChildren<ScrollRect>();
+            if ( scrollRect != null )
+                scrollRect.verticalNormalizedPosition = 1.0f;
+            else
+                Log.Warning( "PassageItem", "Unable to find ScrollRect." );
+        }
 
-		/// <summary>
-		/// Update the confidence tab view. Move Y position of TabItem according to normalized confidence.
-		/// </summary>
-		private void UpdateConfidence()
-		{
-			m_TabItem.Confidence = Confidence;
+        /// <summary>
+        /// Update the confidence tab view. Move Y position of TabItem according to normalized confidence.
+        /// </summary>
+        private void UpdateConfidence()
+        {
+            m_TabItem.Confidence = Confidence;
 
-			double NormalizedConfidence = Confidence - MinConfidence;
-			double ConfidenceRange = MaxConfidence - MinConfidence;
-			float ConfidencePercentage = (float)NormalizedConfidence/(float)ConfidenceRange;
+            double NormalizedConfidence = Confidence - MinConfidence;
+            double ConfidenceRange = MaxConfidence - MinConfidence;
+            float ConfidencePercentage = (float)NormalizedConfidence / (float)ConfidenceRange;
 
-//			m_TabPosY = ((m_MaxYPos - m_MinYPos) * ConfidencePercentage) + m_MinYPos;
-			m_TabPosY = ((m_MaxYPos - m_MinYPos) * ConfidencePercentage) + m_MinYPos;
-			RectTransform TabItemRectTransform = m_TabItem.GetComponent<RectTransform>();
-			TabItemRectTransform.anchoredPosition = new Vector2(m_TabPosX, m_TabPosY);
-		}
-	}
+            m_TabPosY = ((m_MaxYPos - m_MinYPos) * ConfidencePercentage) + m_MinYPos;
+            RectTransform TabItemRectTransform = m_TabItem.GetComponent<RectTransform>();
+            TabItemRectTransform.anchoredPosition = new Vector2(m_TabPosX, m_TabPosY);
+        }
+    }
 }
