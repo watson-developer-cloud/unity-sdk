8f89032b28c74258fd6f233a420c9fb467558ce0
Dogukan Erenel <derenel@us.ibm.com>
2015-12-02 16:16:02 -0600

    * UI element for translation * Microphone widget activate / deactivate function added * Translate widget modified according to bluemix service "conversation" module

--- Assets/Watson/Widgets/TranslationWidget.cs
+++ Assets/Watson/Widgets/TranslationWidget.cs
@@ -39,13 +39,26 @@ namespace IBM.Watson.Widgets
 		[SerializeField]
 		private InputField m_Input = null;
 		[SerializeField]
-		private Dropdown m_DropDownFromLanguage = null;
+		private InputField m_Output = null;
 		[SerializeField]
-		private Dropdown m_DropDownToLanguage = null;
+		private Dropdown m_DropDownSourceLanguage = null;
+		[SerializeField]
+		private Dropdown m_DropDownTargetLanguage = null;
 
+
+
+		private Dictionary<string, string> m_LanguageToIdentify = new Dictionary<string, string> ();
+		private Dictionary<string, List<string>> m_LanguageToTranslate = new Dictionary<string, List<string>> ();
 		private string[] m_LanguagesFrom = null;
-		private string[] m_LanguagesTo = null;
+		//private string[] m_LanguagesTo = null;
 
+		private string m_FromDetectedLanguage = null;
+		private string m_DetectLanguage = "DETECT";
+		private string m_DetectLanguageName = "Detect Language";
+		private string m_DetectedLanguageNameFormat = "Detected: {0}";
+		private string m_DefaultLanguageFromTranslate = "DETECT";
+		private string m_DefaultLanguageToTranslate = "en";
+		private string m_DefaultDomainToUse = "conversational";
 		#endregion
 
         #region Widget interface
@@ -60,9 +73,13 @@ namespace IBM.Watson.Widgets
 		public string FromLanguage{
 			get{
 				string valueToReturn = null;
-				if(m_DropDownFromLanguage != null && m_LanguagesFrom != null && m_LanguagesFrom.Length > m_DropDownFromLanguage.value && m_DropDownFromLanguage.value >= 0){
-					valueToReturn = m_LanguagesTo[m_DropDownFromLanguage.value];
+				if(m_DropDownSourceLanguage != null && !string.IsNullOrEmpty(m_FromDetectedLanguage) && m_DropDownSourceLanguage.value == 0){
+					valueToReturn = m_FromDetectedLanguage;
 				}
+				else if(m_DropDownSourceLanguage != null && m_LanguageToTranslate.Keys != null && m_LanguageToTranslate.Keys.Count > m_DropDownSourceLanguage.value && m_DropDownSourceLanguage.value >= 0){
+					valueToReturn = m_LanguagesFrom[m_DropDownSourceLanguage.value];
+				}
+
 				return valueToReturn;
 			}
 		}
@@ -70,10 +87,22 @@ namespace IBM.Watson.Widgets
 		public string ToLanguage{
 			get{
 				string valueToReturn = null;
-				if(m_DropDownToLanguage != null && m_LanguagesTo != null && m_LanguagesTo.Length > m_DropDownToLanguage.value && m_DropDownToLanguage.value >= 0){
-					valueToReturn = m_LanguagesTo[m_DropDownToLanguage.value];
+				if(m_DropDownTargetLanguage != null && m_LanguageToTranslate != null && FromLanguage != null && m_LanguageToTranslate.ContainsKey(FromLanguage) && m_LanguageToTranslate[FromLanguage].Count > m_DropDownTargetLanguage.value && m_DropDownTargetLanguage.value >= 0){
+					valueToReturn = m_LanguageToTranslate[FromLanguage][m_DropDownTargetLanguage.value];
 				}
 				return valueToReturn;
+			}
+		}
+
+		public bool IsFromLanguageToDetect{
+			get{
+				return string.Equals(FromLanguage, m_DetectLanguage);
+			}
+		}
+
+		public bool IsSameLanguage{	//No need to call translate
+			get{
+				return String.Equals(FromLanguage, ToLanguage);
 			}
 		}
 
@@ -81,7 +110,15 @@ namespace IBM.Watson.Widgets
 
 		private void OnEnable()
 		{
-			m_Translate.GetLanguages( OnGetLanguages );
+			m_Translate.GetLanguages( OnGetLanguagesAndGetModelsAfter );
+			//m_Translate.GetTranslation( "What does the fox say?", "en", "es", OnGetTranslation );
+			//m_Translate.GetTranslation( "What does the fox say?", "en", "fr", OnGetTranslation );
+
+			//m_Translate.GetModel( "en-es", OnGetModel );
+
+			
+
+
 		}
 		
 		private void OnDisable()
@@ -97,7 +134,12 @@ namespace IBM.Watson.Widgets
 			if (m_Translate != null) {
 				if(FromLanguage != null && ToLanguage != null){
 					if (m_Input != null) {
-						m_Translate.GetTranslation (m_Input.text, FromLanguage, ToLanguage, OnGetTranslation);
+						Translate(m_Input.text);
+
+						if(IsFromLanguageToDetect && !string.IsNullOrEmpty(m_Input.text)){	//Identify and translate
+							m_Translate.Identify(m_Input.text, OnIdentifyAndTranslateFromLanguage );
+						}
+
 					}
 					else{
 						Log.Error("TranslationWidget", "OnTranslation - Input is null");
@@ -112,40 +154,207 @@ namespace IBM.Watson.Widgets
 			}
 		}
 
+		private void Translate(string text){
+			if (!string.IsNullOrEmpty (text)) {
+				if (IsSameLanguage || IsFromLanguageToDetect) {	//same language or it is still not detected!
+					SetOutput(text);
+				} else {
+					m_Translate.GetTranslation (m_Input.text, FromLanguage, ToLanguage, OnGetTranslation);
+				}
+			} else {
+				//translation text is empty - clear output text
+				SetOutput("");
+			}
+
+
+		}
+
+		private void SetOutput(string text){
+			if (m_Output != null) {
+				m_Output.text = text;
+			}
+		}
+
 		private void OnGetTranslation( Translations translation )
 		{
 			if ( translation != null && translation.translations.Length > 0 ){
-				Log.Status( "TestTranslate", "Translation: {0}", translation.translations[0].translation );
+				//Log.Status( "TranslationWidget", "OnGetTranslation - Translation: {0}", translation.translations[0].translation );
+				SetOutput( translation.translations[0].translation );
 			}
-			
 		}
 
-		private void OnGetLanguages( Languages languages )
+		private void OnGetLanguagesAndGetModelsAfter( Languages languages )
 		{
-			if ( languages != null )
-			{
-				List<string> listLanguages = new List<string>();
+			if (languages != null && languages.languages.Length > 0) {
+				m_LanguageToIdentify.Clear();
 
-				if(m_DropDownFromLanguage != null){
-					m_DropDownFromLanguage.options.Add(new Dropdown.OptionData("Detect Language"));
-					listLanguages.Add("xx");
-				}
-				foreach( var lang in languages.languages ){
-					if(m_DropDownFromLanguage != null){
-						m_DropDownFromLanguage.options.Add(new Dropdown.OptionData(lang.name));
-					}
-					if(m_DropDownToLanguage != null){
-						m_DropDownToLanguage.options.Add(new Dropdown.OptionData(lang.name));
-					}
-					listLanguages.Add(lang.language);
+				foreach (var lang in languages.languages) {
+					m_LanguageToIdentify[lang.language] = lang.name;
 				}
 
-				m_LanguagesFrom = listLanguages.ToArray();
-				listLanguages.RemoveAt(0);
-				m_LanguagesTo = listLanguages.ToArray();
+				m_LanguageToIdentify[m_DetectLanguage] = m_DetectLanguageName;
+
+				m_Translate.GetModels( OnGetModels );	//To fill dropdown with models to use in Translation
+
+			} else {
+				Log.Error("TranslationWidget", "OnGetLanguages - There is no language to translate. Check the connections and service of Translation Service.");
 			}
 
 		}
+
+		private void OnIdentifyAndTranslateFromLanguage(string lang){
+			//Log.Status( "TranslationWidget", "OnIdentifyAndTranslateFromLanguage as {0}", lang );
+			for (int i = 0; m_LanguagesFrom != null && i < m_LanguagesFrom.Length; i++) {
+				if(String.Equals(m_LanguagesFrom[i], lang)){
+					m_FromDetectedLanguage = lang;
+					m_DropDownSourceLanguage.captionText.text = string.Format(m_DetectedLanguageNameFormat, m_LanguageToIdentify[lang]);
+					m_DropDownSourceLanguage.options[0].text = string.Format(m_DetectedLanguageNameFormat, m_LanguageToIdentify[lang]);
+					ResetTargetLanguageDropDown();
+					Translate(m_Input.text);
+					break;
+				}
+			}
+		}
+
+		private IEnumerator EnableInteractiveDropDown(){
+			yield return  null;
+			m_DropDownSourceLanguage.interactable = true;
+		}
+
+		private void OnGetModels( TranslationModels models )
+		{
+			if ( models != null && models.models.Length > 0)
+			{
+				m_LanguageToTranslate.Clear();
+				List<string> listLanguages = new List<string> ();	//From - To language list to use in translation
+
+				m_DropDownSourceLanguage.options.Clear ();
+				m_DropDownTargetLanguage.options.Clear ();
+				int defaultInitialValueFromLanguage = 0;
+				int defaultInitialValueToLanguage = 0;
+
+				//Adding initial language as detected!
+				listLanguages.Add (m_DetectLanguage);
+				m_LanguageToTranslate.Add(m_DetectLanguage, new List<string>());
+
+				foreach( var model in models.models )
+				{
+					if(string.Equals( model.domain, m_DefaultDomainToUse)){
+
+						if(m_LanguageToTranslate.ContainsKey(model.source)){
+							if(!m_LanguageToTranslate[model.source].Contains(model.target))
+								m_LanguageToTranslate[model.source].Add (model.target);
+						}
+						else{
+							m_LanguageToTranslate.Add(model.source, new List<string>());
+							m_LanguageToTranslate[model.source].Add (model.target);
+						}
+
+						if(!listLanguages.Contains(model.source)){
+							listLanguages.Add(model.source);
+						}
+						if(!listLanguages.Contains(model.target)){
+							listLanguages.Add(model.target);
+						}
+
+						if(!m_LanguageToTranslate[m_DetectLanguage].Contains(model.target))
+							m_LanguageToTranslate[m_DetectLanguage].Add (model.target);
+					}
+
+					//Log.Status( "TestTranslate", "ModelID: {0}, Source: {1}, Target: {2}, Domain: {3}", model.model_id, model.source, model.target, model.domain );
+				}
+
+				//Adding all languages to SourceLanguage dropdown
+				foreach (string itemLanguage in listLanguages) {
+					if(m_LanguageToIdentify.ContainsKey(itemLanguage)){
+						m_DropDownSourceLanguage.options.Add (new Dropdown.OptionData (m_LanguageToIdentify[itemLanguage]));
+					}
+
+					if(String.Equals(m_DefaultLanguageFromTranslate, itemLanguage)){
+						defaultInitialValueFromLanguage = m_DropDownSourceLanguage.options.Count - 1; 
+					}
+
+				}
+				m_LanguagesFrom = listLanguages.ToArray ();
+				
+				m_DropDownSourceLanguage.value = defaultInitialValueFromLanguage;
+
+			}
+		}
+
+		public void DropDownSourceValueChanged(){
+
+			if(m_DropDownSourceLanguage.value != 0){
+				m_DropDownSourceLanguage.options[0].text = m_LanguageToIdentify[m_DetectLanguage];
+				m_FromDetectedLanguage = null;
+			}
+
+			ResetTargetLanguageDropDown ();
+
+			OnTranslation ();
+		}
+
+		public void DropDownTargetValueChanged(){
+			OnTranslation ();
+		}
+
+		public void ResetTargetLanguageDropDown(){
+			if (FromLanguage != null) {
+
+				//Add all languages, because first item is detect language
+				if(m_LanguageToTranslate != null && string.Equals(m_DetectLanguage, FromLanguage)){
+					string languageToPrevious = ToLanguage;
+					m_DropDownTargetLanguage.options.Clear ();
+					m_DropDownTargetLanguage.value = -1;
+					int defaultInitialValueToLanguage = 0;
+
+					foreach (string itemLanguage in m_LanguageToTranslate.Keys) {
+						if(string.Equals(itemLanguage, m_DetectLanguage))
+						   continue;
+
+						m_DropDownTargetLanguage.options.Add (new Dropdown.OptionData (m_LanguageToIdentify[itemLanguage]));
+						
+						if(String.Equals(m_DefaultLanguageToTranslate, itemLanguage)){
+							defaultInitialValueToLanguage = m_DropDownTargetLanguage.options.Count - 1; //first item 'Detect Language' will be removed
+						}
+					}
+
+					m_DropDownTargetLanguage.captionText.text = m_DropDownTargetLanguage.options[0].text;
+					m_DropDownTargetLanguage.value = defaultInitialValueToLanguage;
+
+				}
+				//Add target language corresponding source language
+				else if(m_LanguageToTranslate != null && m_LanguageToTranslate.ContainsKey(FromLanguage) && m_LanguageToTranslate[FromLanguage] != null){
+					string languageToPrevious = ToLanguage;
+					m_DropDownTargetLanguage.options.Clear ();
+					m_DropDownTargetLanguage.value = -1;
+					int defaultInitialValueToLanguage = 0;
+
+					foreach (string itemLanguage in m_LanguageToTranslate[FromLanguage]) {
+						if(string.Equals(itemLanguage, m_DetectLanguage))
+							continue;
+
+						m_DropDownTargetLanguage.options.Add (new Dropdown.OptionData (m_LanguageToIdentify[itemLanguage]));
+
+						if(String.Equals(m_DefaultLanguageToTranslate, itemLanguage)){
+							defaultInitialValueToLanguage = m_DropDownTargetLanguage.options.Count - 1; //first item 'Detect Language' will be removed
+						}
+					}
+
+					m_DropDownTargetLanguage.captionText.text = m_DropDownTargetLanguage.options[0].text;
+					m_DropDownTargetLanguage.value = defaultInitialValueToLanguage;
+
+				}
+				else{
+					Log.Error("TranslationWidget", "ResetTargetLanguageDropDown - There is invalid condition. ");
+				}
+
+			} else {
+				Log.Error("TranslationWidget", "ResetTargetLanguageDropDown - Source language has not been set.");
+			}
+		}
+
+
     }
 
 
