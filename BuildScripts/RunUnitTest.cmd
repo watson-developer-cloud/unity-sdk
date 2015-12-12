f09ffb0b809df1367cbca5b2916c57e066385c7b
Richard Lyle <rolyle@us.ibm.com>
2015-12-12 14:56:19 -0600

    * Removing Config.json from GIT.

--- BuildScripts/RunUnitTest.cmd
+++ BuildScripts/RunUnitTest.cmd
@@ -4,11 +4,13 @@ echo Running UnitySDK UnitTest...
 SET PROJECT=%CD%\..\
 IF "%UNITY_EXE%"=="" SET "UNITY_EXE=C:\Program Files\Unity\Editor\Unity.exe"
 
-"%UNITY_EXE%" -batchmode -executemethod RunUnitTest.All -projectPath %PROJECT%
-IF "%ERRORLEVEL%"=="0" goto success
-echo "UnitTest FAILED!"
-exit /B 1
+""%UNITY_EXE%" -batchmode -executemethod RunUnitTest.All -projectPath %PROJECT%
+IF NOT "%ERRORLEVEL%"=="0" goto error
+
 :success
 echo 'UnitTest COMPLETED!"
 exit /B 0
+:error
+echo "UnitTest FAILED!"
+exit /B 1
 
