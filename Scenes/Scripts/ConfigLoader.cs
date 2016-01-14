ac23766fb457c8fea482844c9b71ea0aecec8e3d
Richard Lyle <rolyle@us.ibm.com>
2016-01-14 10:34:29 -0600

    * Added helper script to make sure the Config is initialized before we attempt to access the services.

---
+++ Scenes/Scripts/ConfigLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using IBM.Watson.DeveloperCloud.Utilities;
+
+//! This helper class makes sure the Watson configuration is fully loaded before we try to access any of the services.
+public class ConfigLoader : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Prefab = null;
+
+	// Use this for initialization
+	IEnumerator Start ()
+    {
+        // wait for the configuration to be loaded first..
+        while (!Config.Instance.ConfigLoaded)
+            yield return null;
+        	
+        // then initiate a prefab after we are done loading the config.
+        GameObject.Instantiate( m_Prefab );
+	}
+	
+}
