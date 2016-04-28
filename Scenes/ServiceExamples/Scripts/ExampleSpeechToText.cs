f7dfb391f56aa94852bd5687f3bd39c0bd12ce79
Ajiemar Santiago <adsantiag@us.ibm.com>
2016-04-28 11:22:33 -0500

    moved service example scenes and scripts into the sdk

---
+++ Scenes/ServiceExamples/Scripts/ExampleSpeechToText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using IBM.Watson.DeveloperCloud.Services.SpeechToText.v1;
+
+public class ExampleSpeechToText : MonoBehaviour
+{
+	[SerializeField]
+	private AudioClip m_AudioClip = new AudioClip(); 
+	private SpeechToText m_SpeechToText = new SpeechToText();
+
+    void Start()
+    {
+		m_SpeechToText.Recognize(m_AudioClip, HandleOnRecognize);
+    }
+
+	void HandleOnRecognize (SpeechResultList result)
+	{
+		if (result != null && result.Results.Length > 0)
+		{
+			foreach( var res in result.Results )
+			{
+				foreach( var alt in res.Alternatives )
+				{
+					string text = alt.Transcript;
+					Debug.Log(string.Format( "{0} ({1}, {2:0.00})\n", text, res.Final ? "Final" : "Interim", alt.Confidence));
+				}
+			}
+		}
+	}
+}
