69e4d44b0ad50974a33673cae37275e84ebd2af7
Ajiemar Santiago <ajiemar@gmail.com>
2016-06-22 14:46:58 -0500

    fixes #90 added personalityInsights to configuration manager, removed underscore from service name

--- Scripts/Editor/ConfigEditor.cs
+++ Scripts/Editor/ConfigEditor.cs
@@ -57,7 +57,9 @@ namespace IBM.Watson.DeveloperCloud.Editor
             new ServiceSetup() { ServiceName = "Language Translator", ServiceAPI = "language-translation/api",
                 URL ="https://console.ng.bluemix.net/catalog/services/language-translation/", ServiceID="LanguageTranslatorV1" },
             new ServiceSetup() { ServiceName = "Natural Language Classifier", ServiceAPI = "natural-language-classifier/api",
-				URL ="https://console.ng.bluemix.net/catalog/natural-language-classifier/", ServiceID="NaturalLanguageClassifierV1" }
+				URL ="https://console.ng.bluemix.net/catalog/natural-language-classifier/", ServiceID="NaturalLanguageClassifierV1" },
+                new ServiceSetup() { ServiceName = "Personality Insights", ServiceAPI = "personality-insights/api",
+                    URL ="https://console.ng.bluemix.net/catalog/services/personality-insights/", ServiceID="PersonalityInsightsV2" }
         };
 
         private const string TITLE = "Watson Unity SDK";
