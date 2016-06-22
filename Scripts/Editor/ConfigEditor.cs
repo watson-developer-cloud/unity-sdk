ff269354e3afd20a1f6735f32c260af4a0ac9e80
Ajiemar Santiago <ajiemar@gmail.com>
2016-06-22 14:37:33 -0500

    fixes #69 added conversation to configuration editor

--- Scripts/Editor/ConfigEditor.cs
+++ Scripts/Editor/ConfigEditor.cs
@@ -57,7 +57,9 @@ namespace IBM.Watson.DeveloperCloud.Editor
             new ServiceSetup() { ServiceName = "Language Translator", ServiceAPI = "language-translation/api",
                 URL ="https://console.ng.bluemix.net/catalog/services/language-translation/", ServiceID="LanguageTranslatorV1" },
             new ServiceSetup() { ServiceName = "Natural Language Classifier", ServiceAPI = "natural-language-classifier/api",
-				URL ="https://console.ng.bluemix.net/catalog/natural-language-classifier/", ServiceID="NaturalLanguageClassifierV1" }
+				URL ="https://console.ng.bluemix.net/catalog/natural-language-classifier/", ServiceID="NaturalLanguageClassifierV1" },
+            new ServiceSetup() { ServiceName = "Conversation", ServiceAPI = "conversation-experimental/api",
+                URL ="https://console.ng.bluemix.net/catalog/services/conversation/", ServiceID="ConversationV1" }
         };
 
         private const string TITLE = "Watson Unity SDK";
