e2c7839c6e287c5df05586fb59740b50397a2a4a
Ajiemar Santiago <ajiemar@gmail.com>
2016-08-18 16:30:29 -0500

    camera streaming into unity editor and android

---
+++ Scripts/UnitTests/TestWebCamera.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using IBM.Watson.DeveloperCloud.Logging;
+using UnityEngine.UI;
+
+public class TestWebCamera : MonoBehaviour
+{
+    private WebCamDevice[] m_WebCams;
+    private int WebCamIndex = 0;
+
+    [SerializeField]
+    private GameObject m_Screen;
+    private WebCamTexture m_WebCamTexture;
+    public RawImage rawimage;
+    public Material material3D;
+    public Button SwitchCameraButton;
+    public Button DetectButton;
+
+    void Awake()
+    {
+        LogSystem.InstallDefaultReactors();
+    }
+
+    void Start()
+    {
+        m_WebCamTexture = new WebCamTexture(); ;
+        m_WebCams = WebCamTexture.devices;
+
+        foreach (WebCamDevice cam in m_WebCams)
+            Log.Debug("TestWebCamera", "cam: {0}", cam);
+
+
+        if (rawimage != null)
+        {
+            rawimage.texture = m_WebCamTexture;
+            rawimage.material.mainTexture = m_WebCamTexture;
+        }
+
+        if(material3D != null)
+            material3D.mainTexture = m_WebCamTexture;
+
+        m_WebCamTexture.Play();
+    }
+
+    public void OnDetectButtonClicked()
+    {
+
+    }
+
+    public void OnSwitchCameraButtonClicked()
+    {
+        if (m_WebCams.Length > 1)
+        {
+            m_WebCamTexture.Stop();
+            m_WebCamTexture.deviceName = (m_WebCamTexture.deviceName == m_WebCams[0].name) ? m_WebCams[1].name : m_WebCams[0].name;
+            m_WebCamTexture.Play();
+        }
+        else
+        {
+            Log.Warning("TestWebCamera", "There is only one camera!");
+        }
+    }
+}
