d55e742b5278347e7dee97e14eac59db6b52508c
Ajiemar Santiago <ajiemar@gmail.com>
2016-10-25 17:36:21 -0500

    formatting, trace recording device to console

--- Examples/ServiceExamples/Scripts/ExampleStreaming.cs
+++ Examples/ServiceExamples/Scripts/ExampleStreaming.cs
@@ -24,145 +24,144 @@ using IBM.Watson.DeveloperCloud.DataTypes;
 
 public class ExampleStreaming : MonoBehaviour
 {
-	private int m_RecordingRoutine = 0;
+  private int m_RecordingRoutine = 0;
+  private string m_MicrophoneID = null;
+  private AudioClip m_Recording = null;
+  private int m_RecordingBufferSize = 2;
+  private int m_RecordingHZ = 22050;
 
+  private SpeechToText m_SpeechToText = new SpeechToText();
 
-	private string m_MicrophoneID = null;
-	private AudioClip m_Recording = null;
-    private int m_RecordingBufferSize = 2;
-	private int m_RecordingHZ = 22050;
+  void Start()
+  {
+    LogSystem.InstallDefaultReactors();
+    Log.Debug("ExampleStreaming", "Start();");
 
+    Active = true;
 
-	private SpeechToText m_SpeechToText = new SpeechToText();
+    StartRecording();
+  }
 
-	void Start()
-	{
-		LogSystem.InstallDefaultReactors();
-		Log.Debug("ExampleStreaming", "Start();");
+  public bool Active
+  {
+    get { return m_SpeechToText.IsListening; }
+    set
+    {
+      if (value && !m_SpeechToText.IsListening)
+      {
+        m_SpeechToText.DetectSilence = true;
+        m_SpeechToText.EnableWordConfidence = false;
+        m_SpeechToText.EnableTimestamps = false;
+        m_SpeechToText.SilenceThreshold = 0.03f;
+        m_SpeechToText.MaxAlternatives = 1;
+        m_SpeechToText.EnableContinousRecognition = true;
+        m_SpeechToText.EnableInterimResults = true;
+        m_SpeechToText.OnError = OnError;
+        m_SpeechToText.StartListening(OnRecognize);
+      }
+      else if (!value && m_SpeechToText.IsListening)
+      {
+        m_SpeechToText.StopListening();
+      }
+    }
+  }
 
-		Active = true;
+  private void StartRecording()
+  {
+    if (m_RecordingRoutine == 0)
+    {
+      UnityObjectUtil.StartDestroyQueue();
+      m_RecordingRoutine = Runnable.Run(RecordingHandler());
+    }
+  }
 
-		StartRecording();
-	}
+  private void StopRecording()
+  {
+    if (m_RecordingRoutine != 0)
+    {
+      Microphone.End(m_MicrophoneID);
+      Runnable.Stop(m_RecordingRoutine);
+      m_RecordingRoutine = 0;
+    }
+  }
 
-	public bool Active
-	{
-		get { return m_SpeechToText.IsListening; }
-		set {
-			if ( value && !m_SpeechToText.IsListening )
-			{
-				m_SpeechToText.DetectSilence = true;
-				m_SpeechToText.EnableWordConfidence = false;
-				m_SpeechToText.EnableTimestamps = false;
-				m_SpeechToText.SilenceThreshold = 0.03f;
-				m_SpeechToText.MaxAlternatives = 1;
-				m_SpeechToText.EnableContinousRecognition = true;
-				m_SpeechToText.EnableInterimResults = true;
-				m_SpeechToText.OnError = OnError;
-				m_SpeechToText.StartListening( OnRecognize );
-			}
-			else if ( !value && m_SpeechToText.IsListening )
-			{
-				m_SpeechToText.StopListening();
-			}
-		}
-	}
+  private void OnError(string error)
+  {
+    Active = false;
 
-	private void StartRecording()
-	{
-		if (m_RecordingRoutine == 0)
-		{
-			UnityObjectUtil.StartDestroyQueue();
-			m_RecordingRoutine = Runnable.Run(RecordingHandler());
-		}
-	}
+    Log.Debug("ExampleStreaming", "Error! {0}", error);
+  }
 
-	private void StopRecording()
-	{
-		if (m_RecordingRoutine != 0)
-		{
-			Microphone.End(m_MicrophoneID);
-			Runnable.Stop(m_RecordingRoutine);
-			m_RecordingRoutine = 0;
-		}
-	}
+  private IEnumerator RecordingHandler()
+  {
+    Log.Debug("ExampleStreaming", "devices: {0}", Microphone.devices);
+    m_Recording = Microphone.Start(m_MicrophoneID, true, m_RecordingBufferSize, m_RecordingHZ);
+    yield return null;      // let m_RecordingRoutine get set..
 
-	private void OnError( string error )
-	{
-		Active = false;
+    if (m_Recording == null)
+    {
+      StopRecording();
+      yield break;
+    }
 
-		Log.Debug("ExampleStreaming", "Error! {0}", error);
-	}
+    bool bFirstBlock = true;
+    int midPoint = m_Recording.samples / 2;
+    float[] samples = null;
 
-	private IEnumerator RecordingHandler()
-	{
-		m_Recording = Microphone.Start(m_MicrophoneID, true, m_RecordingBufferSize, m_RecordingHZ);
-		yield return null;      // let m_RecordingRoutine get set..
+    while (m_RecordingRoutine != 0 && m_Recording != null)
+    {
+      int writePos = Microphone.GetPosition(m_MicrophoneID);
+      if (writePos > m_Recording.samples || !Microphone.IsRecording(m_MicrophoneID))
+      {
+        Log.Error("MicrophoneWidget", "Microphone disconnected.");
 
-		if (m_Recording == null)
-		{
-			StopRecording();
-			yield break;
-		}
+        StopRecording();
+        yield break;
+      }
 
-		bool bFirstBlock = true;
-		int midPoint = m_Recording.samples / 2;
-		float[] samples = null;
+      if ((bFirstBlock && writePos >= midPoint)
+        || (!bFirstBlock && writePos < midPoint))
+      {
+        // front block is recorded, make a RecordClip and pass it onto our callback.
+        samples = new float[midPoint];
+        m_Recording.GetData(samples, bFirstBlock ? 0 : midPoint);
 
-		while (m_RecordingRoutine != 0 && m_Recording != null)
-		{
-			int writePos = Microphone.GetPosition(m_MicrophoneID);
-			if (writePos > m_Recording.samples || !Microphone.IsRecording(m_MicrophoneID))
-			{
-				Log.Error("MicrophoneWidget", "Microphone disconnected.");
-				
-				StopRecording();
-				yield break;
-			}
+        AudioData record = new AudioData();
+        record.MaxLevel = Mathf.Max(samples);
+        record.Clip = AudioClip.Create("Recording", midPoint, m_Recording.channels, m_RecordingHZ, false);
+        record.Clip.SetData(samples, 0);
 
-			if ((bFirstBlock && writePos >= midPoint)
-				|| (!bFirstBlock && writePos < midPoint))
-			{
-				// front block is recorded, make a RecordClip and pass it onto our callback.
-				samples = new float[midPoint];
-				m_Recording.GetData(samples, bFirstBlock ? 0 : midPoint);
+        m_SpeechToText.OnListen(record);
 
-				AudioData record = new AudioData();
-				record.MaxLevel = Mathf.Max(samples);
-				record.Clip = AudioClip.Create("Recording", midPoint, m_Recording.channels, m_RecordingHZ, false);
-				record.Clip.SetData(samples, 0);
+        bFirstBlock = !bFirstBlock;
+      }
+      else
+      {
+        // calculate the number of samples remaining until we ready for a block of audio, 
+        // and wait that amount of time it will take to record.
+        int remaining = bFirstBlock ? (midPoint - writePos) : (m_Recording.samples - writePos);
+        float timeRemaining = (float)remaining / (float)m_RecordingHZ;
 
-				m_SpeechToText.OnListen(record);
+        yield return new WaitForSeconds(timeRemaining);
+      }
 
-				bFirstBlock = !bFirstBlock;
-			}
-			else
-			{
-				// calculate the number of samples remaining until we ready for a block of audio, 
-				// and wait that amount of time it will take to record.
-				int remaining = bFirstBlock ? (midPoint - writePos) : (m_Recording.samples - writePos);
-				float timeRemaining = (float)remaining / (float)m_RecordingHZ;
-				
-				yield return new WaitForSeconds(timeRemaining);
-			}
-			
-		}
+    }
 
-		yield break;
-	}
+    yield break;
+  }
 
-	private void OnRecognize(SpeechRecognitionEvent result)
-	{
-		if (result != null && result.results.Length > 0)
-		{
-			foreach (var res in result.results)
-			{
-				foreach (var alt in res.alternatives)
-				{
-					string text = alt.transcript;
-					Log.Debug("ExampleStreaming", string.Format("{0} ({1}, {2:0.00})\n", text, res.final ? "Final" : "Interim", alt.confidence));
-				}
-			}
-		}
-	}
+  private void OnRecognize(SpeechRecognitionEvent result)
+  {
+    if (result != null && result.results.Length > 0)
+    {
+      foreach (var res in result.results)
+      {
+        foreach (var alt in res.alternatives)
+        {
+          string text = alt.transcript;
+          Log.Debug("ExampleStreaming", string.Format("{0} ({1}, {2:0.00})\n", text, res.final ? "Final" : "Interim", alt.confidence));
+        }
+      }
+    }
+  }
 }
