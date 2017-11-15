11fa18fcbf63d1d1ab88883193a9c6ad6d996670
Ajiemar <ajiemar@gmail.com>
2017-11-15 15:44:24 -0600

    refactored NLC

--- Scripts/UnitTests/TestNaturalLanguageClassifier.cs
+++ Scripts/UnitTests/TestNaturalLanguageClassifier.cs
@@ -29,6 +29,7 @@ using System;
 using FullSerializer;
 using System.Collections.Generic;
 using UnityEngine;
+using IBM.Watson.DeveloperCloud.Connection;
 
 namespace IBM.Watson.DeveloperCloud.UnitTests
 {
@@ -108,22 +109,22 @@ namespace IBM.Watson.DeveloperCloud.UnitTests
             naturalLanguageClassifier = new NaturalLanguageClassifier(credentials);
 
             //  Get classifiers
-            if (!naturalLanguageClassifier.GetClassifiers(OnGetClassifiers))
-                Log.Debug("TestNaturalLanguageClassifier.GetClassifiers()", "Failed to get classifiers!");
+            if (!naturalLanguageClassifier.GetClassifiers(OnGetClassifiers, OnFail))
+                Log.Debug("ExampleNaturalLanguageClassifier.GetClassifiers()", "Failed to get classifiers!");
 
             while (!_getClassifiersTested)
                 yield return null;
 
             if (_classifierIds.Count == 0)
-                Log.Debug("TestNaturalLanguageClassifier.RunTest()", "There are no trained classifiers. Please train a classifier...");
+                Log.Debug("ExampleNaturalLanguageClassifier.Examples()", "There are no trained classifiers. Please train a classifier...");
 
             if (_classifierIds.Count > 0)
             {
                 //  Get each classifier
                 foreach (string classifierId in _classifierIds)
                 {
-                    if (!naturalLanguageClassifier.GetClassifier(classifierId, OnGetClassifier))
-                        Log.Debug("TestNaturalLanguageClassifier.GetClassifier()", "Failed to get classifier {0}!", classifierId);
+                    if (!naturalLanguageClassifier.GetClassifier(OnGetClassifier, OnFail, classifierId))
+                        Log.Debug("ExampleNaturalLanguageClassifier.GetClassifier()", "Failed to get classifier {0}!", classifierId);
                 }
 
                 while (!_getClassifierTested)
@@ -131,14 +132,14 @@ namespace IBM.Watson.DeveloperCloud.UnitTests
             }
 
             if (!_areAnyClassifiersAvailable && _classifierIds.Count > 0)
-                Log.Debug("TestNaturalLanguageClassifier.RunTest()", "All classifiers are training...");
+                Log.Debug("ExampleNaturalLanguageClassifier.Examples()", "All classifiers are training...");
 
             //  Train classifier
 #if TRAIN_CLASSIFIER
             string dataPath = Application.dataPath + "/Watson/Examples/ServiceExamples/TestData/weather_data_train.csv";
             var trainingContent = File.ReadAllText(dataPath);
-            if (!naturalLanguageClassifier.TrainClassifier(_classifierName + "/" + DateTime.Now.ToString(), "en", trainingContent, OnTrainClassifier))
-                Log.Debug("TestNaturalLanguageClassifier.TrainClassifier()", "Failed to train clasifier!");
+            if (!naturalLanguageClassifier.TrainClassifier(OnTrainClassifier, OnFail, _classifierName + "/" + DateTime.Now.ToString(), "en", trainingContent))
+                Log.Debug("ExampleNaturalLanguageClassifier.TrainClassifier()", "Failed to train clasifier!");
 
             while (!_trainClassifierTested)
                 yield return null;
@@ -146,15 +147,15 @@ namespace IBM.Watson.DeveloperCloud.UnitTests
 
 #if DELETE_TRAINED_CLASSIFIER
             if (!string.IsNullOrEmpty(_classifierToDelete))
-                if (!naturalLanguageClassifier.DeleteClassifer(_classifierToDelete, OnDeleteTrainedClassifier))
-                    Log.Debug("TestNaturalLanguageClassifier.DeleteClassifer()", "Failed to delete clasifier {0}!", _classifierToDelete);
+                if (!naturalLanguageClassifier.DeleteClassifer(OnDeleteTrainedClassifier, OnFail, _classifierToDelete))
+                    Log.Debug("ExampleNaturalLanguageClassifier.DeleteClassifer()", "Failed to delete clasifier {0}!", _classifierToDelete);
 #endif
 
             //  Classify
             if (_areAnyClassifiersAvailable)
             {
-                if (!naturalLanguageClassifier.Classify(_classifierId, _inputString, OnClassify))
-                    Log.Debug("TestNaturalLanguageClassifier.Classify()", "Failed to classify!");
+                if (!naturalLanguageClassifier.Classify(OnClassify, OnFail, _classifierId, _inputString))
+                    Log.Debug("ExampleNaturalLanguageClassifier.Classify()", "Failed to classify!");
 
                 while (!_classifyTested)
                     yield return null;
@@ -164,39 +165,37 @@ namespace IBM.Watson.DeveloperCloud.UnitTests
 
             yield break;
         }
-        private void OnGetClassifiers(Classifiers classifiers, string data)
+
+        private void OnGetClassifiers(Classifiers classifiers, Dictionary<string, object> customData)
         {
-            Log.Debug("TestNaturalLanguageClassifier.OnGetClassifiers()", "Natural Language Classifier - GetClassifiers  Response: {0}", data);
+            Log.Debug("ExampleNaturalLanguageClassifier.OnGetClassifiers()", "Natural Language Classifier - GetClassifiers  Response: {0}", customData["json"].ToString());
 
             foreach (Classifier classifier in classifiers.classifiers)
                 _classifierIds.Add(classifier.classifier_id);
 
-            Test(classifiers != null);
             _getClassifiersTested = true;
         }
 
-        private void OnClassify(ClassifyResult result, string data)
+        private void OnClassify(ClassifyResult result, Dictionary<string, object> customData)
         {
-            Log.Debug("TestNaturalLanguageClassifier.OnClassify()", "Natural Language Classifier - Classify Response: {0}", data);
-            Test(result != null);
+            Log.Debug("ExampleNaturalLanguageClassifier.OnClassify()", "Natural Language Classifier - Classify Response: {0}", customData["json"].ToString());
             _classifyTested = true;
         }
 
 #if TRAIN_CLASSIFIER
-        private void OnTrainClassifier(Classifier classifier, string data)
+        private void OnTrainClassifier(Classifier classifier, Dictionary<string, object> customData)
         {
-            Log.Debug("TestNaturalLanguageClassifier.OnTrainClassifier()", "Natural Language Classifier - Train Classifier: {0}", data);
+            Log.Debug("ExampleNaturalLanguageClassifier.OnTrainClassifier()", "Natural Language Classifier - Train Classifier: {0}", customData["json"].ToString());
 #if DELETE_TRAINED_CLASSIFIER
             _classifierToDelete = classifier.classifier_id;
 #endif
-            Test(classifier != null);
             _trainClassifierTested = true;
         }
 #endif
 
-        private void OnGetClassifier(Classifier classifier, string data)
+        private void OnGetClassifier(Classifier classifier, Dictionary<string, object> customData)
         {
-            Log.Debug("TestNaturalLanguageClassifier.OnGetClassifier()", "Natural Language Classifier - Get Classifier {0}: {1}", classifier.classifier_id, data);
+            Log.Debug("ExampleNaturalLanguageClassifier.OnGetClassifier()", "Natural Language Classifier - Get Classifier {0}: {1}", classifier.classifier_id, customData["json"].ToString());
 
             //  Get any classifier that is available
             if (!string.IsNullOrEmpty(classifier.status) && classifier.status.ToLower() == "available")
@@ -205,18 +204,21 @@ namespace IBM.Watson.DeveloperCloud.UnitTests
                 _classifierId = classifier.classifier_id;
             }
 
-            Test(classifier != null);
             if (classifier.classifier_id == _classifierIds[_classifierIds.Count - 1])
                 _getClassifierTested = true;
         }
 
 #if DELETE_TRAINED_CLASSIFIER
-        private void OnDeleteTrainedClassifier(bool success, string data)
+        private void OnDeleteTrainedClassifier(bool success, Dictionary<string, object> customData)
         {
-            Log.Debug("TestNaturalLanguageClassifier.OnDeleteTrainedClassifier()", "Natural Language Classifier - Delete Trained Classifier {0} | success: {1} {2}", _classifierToDelete, success, data);
-            Test(success);
+            Log.Debug("ExampleNaturalLanguageClassifier.OnDeleteTrainedClassifier()", "Natural Language Classifier - Delete Trained Classifier {0} | response: {1}", _classifierToDelete, customData["json"].ToString());
         }
 #endif
+
+        private void OnFail(RESTConnector.Error error, Dictionary<string, object> customData)
+        {
+            Log.Error("ExampleAlchemyLanguage.OnFail()", "Error received: {0}", error.ToString());
+        }
     }
 }
 
