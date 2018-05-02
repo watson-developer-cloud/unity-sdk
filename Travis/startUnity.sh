cac42f5222c70a9bbaeaf93f105a019104d77237
Ajiemar <ajiemar@gmail.com>
2018-05-02 15:39:57 -0500

    log for starting unity

--- Travis/startUnity.sh
+++ Travis/startUnity.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile &
-sleep 15
+echo "Attempting to start Unity"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile $(pwd)/startUnity.log &
+sleep 30
 sudo killall Unity
+
+cat $(pwd)/startUnity.log
+
+if [ $? = 0 ] ; then
+    echo "Moving travis integration tests script SUCCEEDED! Exited with $?"
+    exit 0
+else
+    echo "Moving travis integration tests script FAILED! Exited with $?"
+    exit 1
