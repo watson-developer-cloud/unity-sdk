5fc6c2078f727477a51da047a4261ae40a1373b6
Ajiemar Santiago <ajiemar@gmail.com>
2019-01-13 16:53:32 -0600

    chore(AssistantV2): Updated converter

--- Assistant/v2/Models/MessageContextSkill.cs
+++ Assistant/v2/Models/MessageContextSkill.cs
@@ -17,14 +17,74 @@
 
 
 using FullSerializer;
+using FullSerializer.Internal;
 using IBM.Watson.Utilities;
+using System;
+using System.Collections.Generic;
 
 namespace IBM.Watson.Assistant.V2
 {
-    [fsObject]
+    [fsObject(Converter = typeof(MessageContextSkillConverter))]
+    //[fsObject]
     public class MessageContextSkill
     {
         [fsProperty("user_defined")]
-        public object UserDefined { get; set; }
+        public SerializableDictionary<string, string> UserDefined { get; set; }
+    }
+
+    #region Message Context Skill Converter
+    public class MessageContextSkillConverter : fsConverter
+    {
+        private fsSerializer _serializer = new fsSerializer();
+
+        public override bool CanProcess(Type type)
+        {
+            return type == typeof(MessageContextSkill);
+        }
+
+        public override fsResult TryDeserialize(fsData data, ref object instance, Type storageType)
+        {
+            if (data.IsString == false)
+            {
+                return fsResult.Fail("Type converter requires a string");
+            }
+
+            instance = fsTypeCache.GetType(data.AsString);
+            if (instance == null)
+            {
+                return fsResult.Fail("Unable to find type " + data.AsString);
+            }
+            return fsResult.Success;
+        }
+
+        public override object CreateInstance(fsData data, Type storageType)
+        {
+            return new MessageContextSkill();
+        }
+
+        public override fsResult TrySerialize(object instance, out fsData serialized, Type storageType)
+        {
+            MessageContextSkill messageContextSkill = (MessageContextSkill)instance;
+            serialized = null;
+
+            Dictionary<string, fsData> serialization = new Dictionary<string, fsData>();
+            //Dictionary<string, fsData> userDefinedDict = new Dictionary<string, fsData>();
+
+            fsData tempData = null;
+
+            foreach (KeyValuePair<string, string> kvp in messageContextSkill.UserDefined)
+            {
+                _serializer.TrySerialize(kvp.Value, out tempData);
+                serialization.Add(kvp.Key, tempData);
+            }
+
+            //_serializer.TrySerialize(userDefinedDict, out tempData);
+
+            //serialization.Add("user_defined", tempData);
+            serialized = new fsData(serialization);
+
+            return fsResult.Success;
+        }
+        #endregion
     }
 }
