f85fc746182aa5843599bf87eac2c1b31eea06e7
Ajiemar Santiago <ajiemar@gmail.com>
2019-02-21 14:33:24 -0600

    build: Removed code to move TravisIntegrationTests

--- Travis/installSDK.sh
+++ Travis/installSDK.sh
@@ -19,29 +19,10 @@ git clone -b $TRAVIS_BRANCH https://github.com/watson-developer-cloud/unity-sdk.
 if [ $? = 0 ] ; then
   echo "WDC Unity SDK install SUCCEEDED! Exited with $?"
 
-  echo "Attempting to remove TravisIntegrationTests from Travis directory..."
-  rm Travis/TravisIntegrationTests.cs
-  if [ $? = 0 ] ; then
-    echo "Removing travis build script SUCCEEDED! Exited with $?"
-  else
-    echo "Removing travis build script FAILED! Exited with $?"
-    exit 1
-  fi
-
   echo "Attempting to create Travis/UnityTestProject/Assets/Scripts/Editor/"
   mkdir -p Travis/UnityTestProject/Assets/Scripts/Editor/
   if [ $? = 0 ] ; then
     echo "Creating Travis/UnityTestProject/Assets/Scripts/Editor/ SUCCEEDED! Exited with $?"
-
-    echo "Attempting to move integration tests script..."
-    mv Travis/UnityTestProject/Assets/Watson/Travis/TravisIntegrationTests.cs Travis/UnityTestProject/Assets/Scripts/Editor/TravisIntegrationTests.cs
-    if [ $? = 0 ] ; then
-      echo "Moving travis integration tests script SUCCEEDED! Exited with $?"
-      exit 0
-    else
-      echo "Moving travis integration tests script FAILED! Exited with $?"
-      exit 1
-    fi
   else
     echo "Creating Travis/UnityTestProject/Assets/Scripts/Editor/ FAILED! EXITED WITH $?"
   fi
