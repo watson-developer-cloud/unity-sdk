17434256dd15348df422b3d09215c8de196bed2b
Nanwarin Chantarutai <nanwarin@ibm.com>
2021-09-08 22:47:39 -0700

    feat(assistant-v1): alt_text property added to Image response type

--- Scripts/Services/Assistant/V1/Model/RuntimeResponseGeneric.cs
+++ Scripts/Services/Assistant/V1/Model/RuntimeResponseGeneric.cs
@@ -1,5 +1,5 @@
 /**
-* (C) Copyright IBM Corp. 2019, 2021.
+* (C) Copyright IBM Corp. 2021.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -108,7 +108,7 @@ namespace IBM.Watson.Assistant.V1.Model
         [JsonProperty("typing", NullValueHandling = NullValueHandling.Ignore)]
         public bool? Typing { get; protected set; }
         /// <summary>
-        /// The URL of the image.
+        /// The `https:` URL of the image.
         /// </summary>
         [JsonProperty("source", NullValueHandling = NullValueHandling.Ignore)]
         public string Source { get; protected set; }
@@ -122,6 +122,11 @@ namespace IBM.Watson.Assistant.V1.Model
         /// </summary>
         [JsonProperty("description", NullValueHandling = NullValueHandling.Ignore)]
         public string Description { get; protected set; }
+        /// <summary>
+        /// Descriptive text that can be used for screen readers or other situations where the image cannot be seen.
+        /// </summary>
+        [JsonProperty("alt_text", NullValueHandling = NullValueHandling.Ignore)]
+        public string AltText { get; protected set; }
         /// <summary>
         /// An array of objects describing the options from which the user can choose.
         /// </summary>
